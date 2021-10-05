cfc9dd3d8e21a89a11f63b04d5c70476a008774e
eunbi99 <silverbi99@naver.com>
2021-10-05 13:50:49 +0900

    axios로 데이터 받아오기

--- pages/main.vue
+++ pages/main.vue
@@ -2,16 +2,24 @@
     <div>
         <h1>메인 페이지</h1>
         <p>메인 페이지 입니다.</p>
+        <div>
+            {{products}}
+        </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
 
-export default ({
+export default {
+  data(){
+      return {
+          products: [],
+      }
+  },
   async created(){
       const response = await axios.get('http://localhost:3000/products')
-      console.log(response)
+      this.products = response.data
   }
-})
+}
 </script>
