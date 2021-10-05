96f1b41dbbc3c61f4b26d9974bd765f2752ce0ce
eunbi99 <silverbi99@naver.com>
2021-10-05 20:00:05 +0900

    asyncData를 통한 비동기 데이터 호출

---
+++ components/ProductList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{ products }}
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    async asyncData(){
+        console.log(1)
+        const response = await axios.get('http://localhost:3000/products')
+        /* eslint-disable no-console */
+        console.log(response)
+        /* eslint-enable no-console */
+        const products = response.data
+        return { products } 
+  },
+}
+</script>
