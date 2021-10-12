38637eb8d60d53e9b088c7a485c81c9c41967b6a
eunbi99 <silverbi99@naver.com>
2021-10-12 21:01:50 +0900

    API 모듈화와 asyncData 호출

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,14 +1,28 @@
 <template>
     <div>
         <h1>상세 페이지</h1>
+        <div> 
+            <img :src="product.imageUrl" alt="product.name" />
+            <p>name: {{product.name}}</p>
+            <p>price: {{product.price}}</p>
+        </div>
     </div>
 </template>
 
 <script>
+// import axios from 'axios';
+import { fetchProductById } from '@/api/index'
 
 export default ({
-    created() {
-        console.log(this.$route)
+    async asyncData({ params }){
+        // const id = console.log(params.id)
+        const response = await fetchProductById(params.id)
+        const product = response.data
+        return { product }
     },
+    // created() {
+    //     const id = console.log(this.$route.params.id)
+    //     fetchProductById();
+    // },
 })
 </script>
