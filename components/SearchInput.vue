cc52d9b4e92be7c6bc7705e8e050eaf6f6dea62c
eunbi99 <silverbi99@naver.com>
2022-02-14 22:55:00 +0900

    검색창 기능 마무리

--- components/SearchInput.vue
+++ components/SearchInput.vue
@@ -1,21 +1,23 @@
 <template>
     <div>
-        <input 
-            type="text" 
-            :value="value" 
-            @input="$emit('input', $event.target.value)"/> <!--input 태그에 걸려있는 value값들이 바뀔 때 마다 input 이벤트를 위로 올림-->
-        <button>search</button>
+        <input
+          type="text"
+          :value="value"
+          @input="$emit('input',$event.target.value)" />
+        <button type="button" @click="$emit('search')">search</button>
     </div>
 </template>
 
 <script>
 
 export default({
-    props: {
-        value: {
-            type: String,
-            default: () => '',
-        },
-    },
+  // 상위 컴포넌트인 index.vue에서 v-model로 값을 내려주면 props에서 value로 받아 searchInput.vue의 value로 값이 들어간다!
+  props: {
+    value: {
+      type:String,
+      default: () => '',
+    }
+  }
+
 })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,30 @@
 <template>
         <div class="app">
           <main>
-            <!-- :search-keyword='searchKeyword'
-              @input="updateSearchKeyword" 대신 v-model을 사용했음-->
-            <SearchInput v-model="searchKeyword" />
+            <!--
+              :search-keyword='searchKeyword'
+              @input="updateSearchKeyword"
+              대신 v-model을 사용함으로서, 메소드를 하나 줄일 수 있다.
+              단점: v-model은 한국어 입력을 다룰 때ㅡ 한 글자에 대한 입력이 끝나야지만 동기화가 되기 때문에 한계점이 있다.
+            -->
+            <SearchInput
+              v-model="searchKeyword"
+              @search="searchProducts"
+            />
             <ul>
                 <li v-for="product in products" :key="product.id" class="item flex" @click="moveToDetailPage(product.id)">
                     <img class="product-image" :src="product.imageUrl" :alt="product.name" />
                     <p>{{product.name}}</p>
                     <span>{{product.price}}</span>
-                </li>  
+                </li>
             </ul>
-          </main>  
+          </main>
         </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { fetchProductsByKeyword } from "../api";
 import SearchInput from '@/components/SearchInput.vue'
 
 export default {
@@ -28,10 +36,10 @@ export default {
         console.log(response)
         /* eslint-enable no-console */
         const products = response.data.map((item) =>({
-                ...item,
-                imageUrl:`${item.imageUrl}?random=${Math.random()}`,
+          ...item,
+          imageUrl:`${item.imageUrl}?random=${Math.random()}`,
         }))
-        return { products } 
+        return { products }
   },
   data() {
     return {
@@ -42,9 +50,14 @@ export default {
     moveToDetailPage(id) {
       this.$router.push(`detail/${id}`)
     },
-    updateSearchKeyword(keyword){
-      this.searchKeyword = keyword
-    },
+    async searchProducts() {
+      const response = await fetchProductsByKeyword(this.searchKeyword);
+      console.log(response);
+      this.products = response.data.map((item) =>({
+        ...item,
+        imageUrl:`${item.imageUrl}?random=${Math.random()}`,
+      }))
+    }
   }
 //   data(){
 //       return {
